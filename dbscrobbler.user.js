5a545a2d74ca60bcf309dd02d98a0192cd9b374c
justan <ckr189@gmail.com>
2011-03-20 00:03:43 +0800

    豆瓣电台scrobbler新增过滤广告功能

--- dbscrobbler.user.js
+++ dbscrobbler.user.js
@@ -6,7 +6,8 @@ var meta = <><![CDATA[
 // @include        http://douban.fm/
 // @include        http://douban.fm/?*
 // @require        https://github.com/justan/gmscrobber/raw/master/simple_scrobbler_user.js
-// @version        0.1
+// @version        0.1.1
+// @changelog      新增过滤广告功能
 // @uso:script     98833
 // ==/UserScript==
 ]]></>.toString();
@@ -32,21 +33,22 @@ var douban = function(){
 			song.album = song.albumtitle;
 			song.duration = song.len || 180;
 			
-			if(song.artist == "豆瓣电台"){
-				return;
-			}else if(/\.{3}$/.test(song.album)){
-				log("一个省略的专辑名...");
-				if(o.type == cmds.start){
-					song.album = "";
-					setTimeout(function(){getAlbum(song.aid, function(at){
-						sc.song.album = at;
-						log("新的专辑名是: " + at);
-					})}, 0);
+			setTimeout(function(){
+				if(song.ssid == null || song.subtype == "T"){
+					log("广告, 略过...");
+					return;
+				}else if(/\.{3}$/.test(song.album)){
+					log("一个省略的专辑名...");
+					if(o.type == cmds.start){
+						song.album = "";
+						getAlbum(song.aid, function(at){
+							sc.song.album = at;
+							log("新的专辑名是: " + at);
+						});
+					}
 				}
-			}
-			switch(o.type){
-			case cmds.start:
-				setTimeout(function(){
+				switch(o.type){
+				case cmds.start:
 					sc.nowPlaying(song);
 					sc.getInfo(song, function(p){
 						log(JSON.stringify(p));
@@ -63,20 +65,21 @@ var douban = function(){
 						}
 						document.getElementById("radioplayer").parentNode.title = "在last.fm中记录 " + p.len + " 次";
 					});
-				}, 0);
-				break;
-			case cmds.end:
-				setTimeout(function(){sc.scrobble()}, 0);
-				break;
-			case cmds.like:
-				setTimeout(function(){sc.love()}, 0);
-				break;
-			case cmds.unlike:
-				setTimeout(function(){sc.unlove()}, 0);
-				break;
-			default:
-				break;
-			}
+					break;
+				case cmds.end:
+					sc.scrobble();
+					break;
+				case cmds.like:
+					sc.love();
+					break;
+				case cmds.unlike:
+					sc.unlove();
+					break;
+				default:
+					break;
+				}
+			
+			}, 0);
 			
 			return ex.apply(this, arguments);
 			//ex(o);
