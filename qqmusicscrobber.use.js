377aadb37c52821574bf2d52b5ffb92633df9846
justan <ckr189@gmail.com>
2012-06-13 23:31:46 +0800

    在 y.qq.com 上新建用户脚本

---
+++ qqmusicscrobber.use.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        QQ音乐 online scrobbler
+// @namespace   http://gmscrobber.whosemind.net
+// @description 记录qq在线音乐到 last.fm
+// @match       http://y.qq.com/*
+// @require     https://raw.github.com/justan/gmscrobber/master/simple_scrobbler_user.js
+// @version     0.0.1
+// ==/UserScript==
