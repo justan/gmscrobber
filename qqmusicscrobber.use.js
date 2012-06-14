e1caf7deef78d6c5c47ba68a3d236f82724b331a
cole <ckr189@gmail.com>
2012-06-14 14:36:16 +0800

    校正匹配 url, 排除 iframe url

--- qqmusicscrobber.use.js
+++ qqmusicscrobber.use.js
@@ -2,41 +2,19 @@
 // @name        QQ音乐 online scrobbler
 // @namespace   http://gmscrobber.whosemind.net
 // @description 记录qq在线音乐到 last.fm
-// @match       http://y.qq.com/*
-// @exclude     http://y.qq.com/y/static/index.html
+// @match       http://y.qq.com/
+// @match       http://y.qq.com/?*
+// @match       http://y.qq.com/#*
+// @exclude     http://y.qq.com/y/*
 // @require     https://raw.github.com/justan/gmscrobber/master/simple_scrobbler_user.js
 // @version     0.0.1
 // ==/UserScript==
 
 var init = function(){
   log('init');
-  qmusicWatch();
 };
 
 var scrobber = new Scrobbler({
   name: 'QQ 音乐',
   ready: init
 });
-
-var qmusicWatch = function(){
-  var fn = function(){
-    setInterval(function(){
-      var song = getSongInfo()
-      log(JSON.stringify(song));
-    }, 2000);
-  };
-  var getSongInfo = function(){
-    var song = {};
-    var songinfo = document.getElementById('divsonginfo');
-    song.title = songinfo.getElementsByClassName('music_name')[0].title;
-    song.artist = songinfo.getElementsByClassName('singer_name')[0].title;
-    song.duration = timeParse(document.getElementById('ptime').innerHTML);
-    song.album = songinfo.getElementsByClassName('album_pic')[0].title;
-    return song;
-  };
-  var timeParse = function(timeStr){
-    var ts = timeStr.split(':');
-    return ts[0] * 60 + ts[1] * 1;
-  };
-  return fn;
-}();
