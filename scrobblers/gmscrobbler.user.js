38270f6f89a36c82720e7ef519d72a37cf4f091a
justan <ckr189@gmail.com>
2018-10-21 18:02:05 +0800

    更新 https://play.google.com/music/listen

--- scrobblers/gmscrobbler.user.js
+++ scrobblers/gmscrobbler.user.js
@@ -5,38 +5,59 @@
 // @include        http://play.google.com/music/listen*
 // @include        https://play.google.com/music/listen*
 // @require        https://raw.githubusercontent.com/justan/lrc/master/lrc.js
-// @require        http://justan.github.io/gmscrobber/simple_scrobbler_user.js
-// @version        0.2.1
+// @require        https://justan.github.io/gmscrobber/simple_scrobbler_user.js
+// @version        0.3.0
 // @grant          GM_getValue
 // @grant          GM_setValue 
-// @grant          GM_deleteValue 
+// @grant          GM_deleteValue
+// @grant          unsafeWindow
+// @grant          GM_log
 // @grant          GM_xmlhttpRequest 
 // @grant          GM_registerMenuCommand 
 // @grant          unsafeWindow
 // ==/UserScript==
 
-var meta = uso.metaParse(GM_info.scriptMetaStr);
-
-var sc = new Scrobbler({
+var scrobber = new Scrobbler({
   name: "Google Play Music"
 , ready: function() {
     this.on('nowplaying', function() {
       this.getInfo(this.song, function(info) {
         document.getElementById('player').title = '在 last.fm 中记录: ' + info.len + ' 次';
+        var localInfo = getSongInfo()
+        if(info.islove === '1' && !localInfo.isLove || info.islove === '0' && localInfo.isLove) {
+            document.querySelector('#player [icon="sj:thumb-up-outline"]').click()
+        }
       });
+
+      hasStarSync || starSync()
     });
     
     this.setSongInfoFN(getSongInfo);
   }
 });
 
+var hasStarSync = false
+
+var starSync = function() {
+    document.querySelector('#player [icon="sj:thumb-up-outline"]').addEventListener('click', function() {
+      var info = getSongInfo();
+      if(!info.isLove){
+          scrobber.love();
+      }else{
+          scrobber.unlove();
+      }
+    }, false)
+    hasStarSync = true
+}
+
 var getSongInfo = function() {
   return {
-    title: q('#playerSongTitle>.tooltip')
+    title: q('#currently-playing-title')
   , artist: q('#player-artist')
   , album: q('.player-album')
-  , playTime: uso.timeParse(q('#currentTime'))
-  , duration: uso.timeParse(q('#duration'))
+  , playTime: uso.timeParse(q('#time_container_current'))
+  , duration: uso.timeParse(q('#time_container_duration'))
+  , isLove: document.querySelector('#player [icon="sj:thumb-up-outline"]').getAttribute('aria-label') !== "Thumb-up"
   }
 };
 
@@ -44,73 +65,3 @@ var q = function() {
   return document.querySelector.apply(document, arguments).innerHTML;
 };
 
-
-//TODO 红心同步
-(function() {
-  var plug = function() {/*
-    (function(){
-      var XHR = XMLHttpRequest.prototype
-        , _open = XHR.open
-        , _send = XHR.send
-        ;
-      XHR.open = function(){
-        this.__type__ = arguments[0];
-        this.__url__ = arguments[1];
-        return _open.apply(this, arguments);
-      };
-      XHR.send = function(){
-        this.__data__ = arguments[0];
-        send(this);
-        return _send.apply(this, arguments);
-      };
-      
-      var list = {}, send = function(xhr){
-        var path = xhr.__url__.replace(/\?.+/, '');
-        if(list[path]){
-          list[path].forEach(function(fn){
-            fn.apply(null, [xhr.__url__, xhr.__data__]);
-          });
-        }
-      };
-      window['whosemind'] = {
-        list: list
-      };
-    })();
-  */}.toString().replace(/(?:^function \(\) {\/\*|\*\/\}$)/g, '');
-  
-  var script = document.createElement('script');
-  
-  script.innerHTML = plug;
-  document.head.appendChild(script);
-  
-  var onSend = function(fn, path){
-    var list = unsafeWindow.whosemind.list;
-    list[path] = list[path] || [];
-    list[path].push(fn);
-  };
-  
-  0 && onSend(function(url, data){
-      var d = decodeURIComponent(data.replace(/json=/, '')),
-        info = JSON.parse(d), entries = info.entries, rate, song = {}, _ele;
-      if(entries.length == 1 && !entries[0].creationDate){//rate
-        log(d);
-        rate = entries[0].rating;
-        id = entries[0].id;
-        _ele = document.querySelector('[data-id="' + id + '"]');
-        song.title = _ele.children[0].textContent;
-        song.artist = _ele.children[2].textContent;
-        setTimeout(function(){
-          switch(rate){
-          case 0:
-          case 1:
-            sc.unlove(song);
-            break;
-          case 5:
-            sc.love(song);
-            break;
-          }
-        }, 0);
-      }
-    }, 'services/modifyentries');
-  
-})();
