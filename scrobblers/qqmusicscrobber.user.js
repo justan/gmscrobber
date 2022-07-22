b962df5145ca308d1c6ae938a5f15a0fc94441da
justan <ckr189@gmail.com>
2022-07-22 23:49:34 +0800

    Update qqmusicscrobber.user.js

--- scrobblers/qqmusicscrobber.user.js
+++ scrobblers/qqmusicscrobber.user.js
@@ -2,10 +2,12 @@
 // @name        QQ音乐 online scrobbler
 // @namespace   http://gmscrobber.whosemind.net
 // @description 记录qq在线音乐到 last.fm
+// @match       https://y.qq.com/n/ryqq/player
+// @match       https://y.qq.com/n/ryqq/player?*
 // @match       https://y.qq.com/portal/player.html
 // @match       https://y.qq.com/portal/player.html?*
 // @require     https://justan.github.io/gmscrobber/simple_scrobbler_user.js
-// @version     0.1.0
+// @version     0.1.1
 // @author      justan
 // @grant       GM_getValue
 // @grant       GM_setValue 
@@ -18,9 +20,9 @@
 // ==/UserScript==
 
 var init = function(){
-  log('init');
+  log('gmscrobbler init');
   scrobber.setSongInfoFN(getSongInfo);
-  document.getElementById('progress').addEventListener('click', function(e){
+  document.querySelector('.player_progress').addEventListener('click', function(e){
     var oldTime = getSongInfo().playTime;
     setTimeout(function(){
       var newTime = getSongInfo().playTime;
@@ -30,7 +32,7 @@ var init = function(){
   }, true);
   
   scrobber.on('nowplaying', function(){
-    var loveEle = document.querySelector('#opbanner a.btn_big_like');
+    var loveEle = document.querySelector('a.btn_big_like');
     loveEle.addEventListener('click', function(e){
       var info = getSongInfo();
       if(!info.isLove){
@@ -41,7 +43,7 @@ var init = function(){
     }, false);
     scrobber.getInfo(scrobber.song, function(info){
       var song = getSongInfo();
-      document.getElementById('opbanner').title = '在 last.fm 中记录: ' + info.len + ' 次';      
+      document.querySelector('.player__ft').title = '在 last.fm 中记录: ' + info.len + ' 次';
       //同步 last.fm 红星歌曲到 qq music
       if(info.islove == '1' && !song.isLove || info.islove == '0' && song.isLove){
         loveEle.click();
@@ -57,13 +59,13 @@ var scrobber = new Scrobbler({
 
 var getSongInfo = function(){
   var song = {};
-  var times = document.querySelector('#time_show').innerHTML.split(' / ');
-  song.title = document.querySelector('#song_name a').title.replace(/\([^\)]*\)/, '');
-  song.artist = document.querySelector('#singer_name a').title;
+  var times = document.querySelector('.player_music__time').innerHTML.split(' / ');
+  song.title =document.querySelector('.song_info__name a').innerText.replace(/\([^\)]*\)/, '');
+  song.artist = document.querySelector('.mod_song_info .playlist__author').title;
   song.duration = timeParse(times[1]);
   song.playTime = timeParse(times[0]);
-  song.album = document.querySelector('#album_name a').title;
-  song.isLove = !/^喜欢/.test(document.querySelector('#opbanner a.btn_big_like').title);
+  song.album = document.querySelector('.song_info__album a').innerText;
+  song.isLove = document.querySelector('a.btn_big_like').className.includes('btn_big_like--like');
   return song;
 };
 
